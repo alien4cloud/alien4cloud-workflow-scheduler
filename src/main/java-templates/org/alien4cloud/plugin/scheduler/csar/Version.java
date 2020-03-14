d51ddbc30f1ef708cb458d7ed47da96ab9ffc333
Xavier DEGENNE <xavier.degenne@gmail.com>
2020-03-14 09:42:09 +0100

    [ALIEN-3405] fix version of CSARs

--- src/main/java-templates/org/alien4cloud/plugin/scheduler/csar/Version.java
+++ src/main/java-templates/org/alien4cloud/plugin/scheduler/csar/Version.java
@@ -4,5 +4,5 @@ package org.alien4cloud.plugin.scheduler.csar;
  * Contains plugin version.
  */
 public class Version {
-    public static final String SCHEDULER_CSAR_VERSION = "${project.version}";
+    public static final String SCHEDULER_CSAR_VERSION = "${org.alien4cloud.scheduling.version}";
 }
